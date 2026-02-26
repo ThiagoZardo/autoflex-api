c14bddf550e2f5c8cafa852696d12bdb662e3a6c
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2026-02-26 01:09:02 -0300

    feat: Configure application bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { RawMaterialsModule } from './raw-materials/raw-materials.module';
+import { ProductRawMaterialsModule } from './product-raw-materials/product-raw-materials.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false, // Use with caution in production
+    }),
+    ProductsModule,
+    RawMaterialsModule,
+    ProductRawMaterialsModule,
+  ],
 })
 export class AppModule {}
