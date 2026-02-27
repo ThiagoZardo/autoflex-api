69dbc0826ed565bb8b31ebcad793199b7bb9bbf0
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2026-02-27 16:18:20 -0300

    doc: Init creation of doc with swagger

--- src/raw-materials/dto/update-raw-material.dto.ts
+++ src/raw-materials/dto/update-raw-material.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRawMaterialDto } from './create-raw-material.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateRawMaterialDto extends PartialType(CreateRawMaterialDto) {}
+export class UpdateRawMaterialDto extends PartialType(CreateRawMaterialDto) {
+  @ApiProperty({
+    example: 'Flour',
+    description: 'Raw material name',
+  })
+  name?: string;
+
+  @ApiProperty({
+    example: 100,
+    description: 'Stock quantity available',
+  })
+  stockQuantity?: number;
+}
